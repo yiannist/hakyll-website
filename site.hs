0591387ffd020cf2e8445919292c66ded724d711
Yiannis Tsiouris <gtsiour@softlab.ntua.gr>
2013-04-17 11:22:08 +0300

    Re-indent and merge matches for static content

--- site.hs
+++ site.hs
@@ -4,39 +4,25 @@ import Hakyll
 
 main :: IO ()
 main = hakyll $ do
-  -- Compress CSS
-  match "css/*" $ do
-    route idRoute
-    compile compressCssCompiler
+    -- Compress CSS
+    match "css/*" $ do
+        route idRoute
+        compile compressCssCompiler
 
-  -- Copy images
-  match "images/*" $ do
-    route idRoute
-    compile copyFileCompiler
+    -- Copy static content
+    match ("images/*" .||. "favicon.ico" .||. "files/**" .||. "js/*") $ do
+        route idRoute
+        compile copyFileCompiler
 
-  match "favicon.ico" $ do
-    route idRoute
-    compile copyFileCompiler
+    -- Read templates
+    match "templates/*" $ compile templateCompiler
 
-  -- Copy files (deep)
-  match "files/**" $ do
-    route idRoute
-    compile copyFileCompiler
-
-  -- Copy JavaScript
-  match "js/*" $ do
-    route idRoute
-    compile copyFileCompiler
-
-  -- Read templates
-  match "templates/*" $ compile templateCompiler
-
-  -- Render some static pages
-  match (fromList pages) $ do              
-    route $ setExtension "html"
-    compile $ pandocCompiler
-      >>= loadAndApplyTemplate "templates/default.html" defaultContext
-      >>= relativizeUrls
+    -- Render some static pages
+    match (fromList pages) $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
   where pages = [ "index.md"
                 , "cv.md"
                 , "pubs.md"
